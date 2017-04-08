4bc373797db7fef9b7024accbfab7daa1c0dd424
benbotto <benbotto@users.noreply.github.com>
2017-04-08 13:25:46 -0700

    Driver implemented.

--- driver/MSSQLDriver.js
+++ driver/MSSQLDriver.js
@@ -1,11 +1,11 @@
 'use strict';
 
 require('insulin').factory('ndm_MSSQLDriver',
-  ['mssql', 'ndm_MSSQLDataContext', 'ndm_informationSchemaDatabase',
-  'ndm_MSSQLSchemaGenerator', 'ndm_Database'],
+  ['deferred', 'mssql', 'ndm_MSSQLDataContext', 'ndm_MSSQLSchemaGenerator',
+  'ndm_Database'],
   ndm_MSSQLDriverProducer);
 
-function ndm_MSSQLDriverProducer(mssql, MSSQLDataContext, isDB,
+function ndm_MSSQLDriverProducer(deferred, mssql, MSSQLDataContext,
   MSSQLSchemaGenerator, Database) {
   /**
    * A MSSQL Driver for the node-data-mapper ORM.
@@ -19,15 +19,7 @@ function ndm_MSSQLDriverProducer(mssql, MSSQLDataContext, isDB,
      */
     constructor(conOpts) {
       this.dataContext = null;
-
-      // Copy the connection options locally and add the queryFormat function
-      // (node-data-mapper uses :param format).
-      this.conOpts = Object.assign({}, conOpts, {queryFormat});
-
-      // Create a DataContext instance for the information_schema database.
-      const isConOpts = Object.assign({}, this.conOpts, {database: 'information_schema'});
-      const isCon     = mssql.createConnection(isConOpts);
-      const isDC      = new MSSQLDataContext(isDB, isCon);
+      this.conOpts     = conOpts;
 
       /**
        * A MSSQLSchemaGenerator instance.  The user can attach event handlers
@@ -36,38 +28,25 @@ function ndm_MSSQLDriverProducer(mssql, MSSQLDataContext, isDB,
        * @name MSSQLDriver#generator
        * @public
        */
-      this.generator = new MSSQLSchemaGenerator(isDC);
-
-      // Custom parameter escaping.  Comes directly from here:
-      // https://github.com/mssqljs/mssql#custom-format
-      function queryFormat(query, values) {
-        if (!values)
-          return query;
-
-        return query.replace(/\:(\w+)/g, (txt, key) => {
-          // "this" is a Connection instance with an escape() function.
-          if (values.hasOwnProperty(key)) {
-            return this.escape(values[key]);
-          }
-
-          return txt;
-        });
-      }
+      this.generator = new MSSQLSchemaGenerator(mssql);
     }
 
     /**
      * Initialize the database schema, connection pool, and Datacontext
      * instance.
+     * @param {String} [schema='dbo'] - The schema for which the schema should
+     * be generated.
      * @return {Promise<DataContext>} A Promise that is resolved with a
      * DataContext instance, which can be used for querying the database.
      */
-    initialize() {
-      // Generate the schema, then create the DataContext instance.
-      return this.generator
-        .generateSchema(this.conOpts.database)
+    initialize(schema = 'dbo') {
+      // Connect.
+      return deferred(mssql.connect(this.conOpts))
+        // Generate the schema, then create the DataContext instance.
+        .then(() => this.generator.generateSchema(this.conOpts.database, 'dbo'))
         .then(schema => {
           // The DataContext instance is stored locally for convenient access.
-          this.dataContext = new MSSQLDataContext(new Database(schema), mssql.createPool(this.conOpts));
+          this.dataContext = new MSSQLDataContext(new Database(schema), mssql);
 
           // Resolve with the DC.
           return this.dataContext;
